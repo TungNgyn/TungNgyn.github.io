f66589050fc21e0479987b70cff9f08a458db1f0
Tung <64102492+TungNgyn@users.noreply.github.com>
2023-03-06 10:32:59 +0100

    changed translation file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,88 +1,15 @@
 window.addEventListener("load", event => {
-    // document.querySelector("a.home").addEventListener("click", event => {
-    //     showHome();
-    // });
+    // Translations  
+    document.querySelector('#deTranslator').addEventListener("click", event => {
+        document.querySelector("#enTranslator").style = "display: inherit;";
+        document.querySelector("#deTranslator").style = "display: none;";
+        tinyi18n.getLang() == 'en' ? tinyi18n.setLang('de') : tinyi18n.setLang('en')
+    });
 
-    // document.querySelector("a.projects").addEventListener("click", event => {
-    //     showProjects();
-    // });
-    
-    // document.querySelector("a.contact").addEventListener("click", event => {
-    //     showContact();
-    // });
-    
+    document.querySelector('#enTranslator').addEventListener("click", event => {
+        document.querySelector("#deTranslator").style = "display: inherit;";
+        document.querySelector("#enTranslator").style = "display: none;";
+        tinyi18n.getLang() == 'en' ? tinyi18n.setLang('de') : tinyi18n.setLang('en')
+    });
 });
 
-// function showHome() {
-//     document.querySelector("article.home").style.display = 'flex';
-//     document.querySelector("article.projects").style.display = 'none';
-//     document.querySelector("article.contact").style.display = 'none';
-// };
-
-// function showProjects() {
-//     document.querySelector("article.home").style.display = 'none';
-//     document.querySelector("article.projects").style.display = 'flex';
-//     document.querySelector("article.contact").style.display = 'none';
-// };
-
-// function showContact() {
-//     document.querySelector("article.home").style.display = 'none';
-//     document.querySelector("article.projects").style.display = 'none';
-//     document.querySelector("article.contact").style.display = 'flex';
-// };
-
-// Translations  
-var lng = 'en';
-function translate(lng, tagAttr){
-    var translate = new Translate();
-    translate.init(tagAttr, lng);
-    translate.process();
-
-    if (lng == 'en') {
-        document.querySelector("#deTranslator").addEventListener("click", event => {
-            this.translate('de', 'lng-tag');
-            document.querySelector("#enTranslator").style = "display: inherit;";
-            document.querySelector("#deTranslator").style = "display: none;";
-        })
-    }
-    if (lng == 'de') {
-        document.querySelector("#enTranslator").addEventListener("click", event => {
-            this.translate('en', 'lng-tag');
-            document.querySelector("#enTranslator").style = "display: none;";
-            document.querySelector("#deTranslator").style = "display: inherit;";
-        })
-    }   
-}
-
-function Translate() {
-    // initialization
-    this.init = function(attribute, lng) {
-        this.attribute = attribute;
-        this.lng = lng;
-    }
-
-    // translate
-    this.process = function() {
-        _self = this;
-        var xrhFile = new XMLHttpRequest();
-        // loading file
-        xrhFile.open("GET", "assets/translations/"+this.lng+".json", false);
-        xrhFile.onreadystatechange = function () {
-            if(xrhFile.readyState === 4) {
-                if(xrhFile.status === 200 || xrhFile.status == 0) {
-                    var LngObject = JSON.parse(xrhFile.responseText);
-                    var allDom = document.getElementsByTagName("*");
-                    for(var i =0; i < allDom.length; i++){
-                        var elem = allDom[i];
-                        var key = elem.getAttribute(_self.attribute);
-                        if(key != null) {
-                            elem.innerHTML = LngObject[key]  ;
-                        }
-                    }
-                
-                }
-            }
-        }
-        xrhFile.send();
-    }
-}
--- assets/scripts/typed.js
+++ assets/scripts/typed.js
@@ -1,20 +1,17 @@
 window.addEventListener("load", event => {
+    // Code text
+    const java = 
+    'public class ShowcaseApp {\n'+
+        '    public static void main(String[] args) {\n' +
+        '        system.out.println("Hello");\n' +
+        '    }\n' +
+    '}';
+
+
     // Code Typing
     var options = {
         strings: [java],
         typeSpeed: 100
     };
     var typed = new Typed('article.code output', options);
-
-
 });
-
-
-
-// Code text
-const java = 
-'public class ShowcaseApp {\n'+
-    '    public static void main(String[] args) {\n' +
-    '        system.out.println("Hello");\n' +
-    '    }\n' +
-'}';
