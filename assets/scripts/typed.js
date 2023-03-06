8a0d997a236bf5ca57348521272c1607aac3dfdc
Tung Nguyen <Nguyen.Tung@outlook.de>
2023-03-06 23:42:47 +0100

    linux terminal

--- assets/scripts/typed.js
+++ assets/scripts/typed.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", event => {
     // Code text
+    let window = 'article.code output';
     const java1 = ('public class ShowcaseApp {\n   ^1000 public static void main (String[] args) {\n      ^1000  system.out.println("Hello, World");\n    }\n }');
     // const java2 = ('public class ApplicationContainer {\n    public static void main (final String[] args) { \n        help');
 
@@ -13,6 +14,9 @@ window.addEventListener("load", event => {
     
     const flutter1 = ('void main() {\n   ^1000 runApp(MainApp());\n }\n\n class MainApp extends StatelessWidget {\n   ^1000 const MainApp({super.key});\n\n  ^1000  @override\n  ^1000  Widget build(BuildContext context) {\n      ^1000  return Scaffold();\n    }\n }');
     
+
+    const html1 = ("<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"UTF-8\">\n    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n    <title>Document</title>\n</head>");
+
     // Code Typing
     var code = [java1];
     var options = {
@@ -23,12 +27,11 @@ window.addEventListener("load", event => {
         smartBackspace: true,
         typeSpeed: 100
     };
-    var typed = new Typed('article.code output', options);
+    var typed = new Typed(window, options);
 
     
-    // Skillchange
+    // Language change
     document.querySelector("img.java").addEventListener("click", event => {
-        typed.stop();
         typed.destroy();
         code = [java1];
         options = {
@@ -39,10 +42,9 @@ window.addEventListener("load", event => {
             smartBackspace: true,
             typeSpeed: 100
         };
-        typed = new Typed('article.code output', options);
+        typed = new Typed(window, options);
     });
     document.querySelector("img.kotlin").addEventListener("click", event => {
-        typed.stop();
         typed.destroy();
         code = [kotlin1];
         options = {
@@ -53,10 +55,9 @@ window.addEventListener("load", event => {
             smartBackspace: true,
             typeSpeed: 100
         };
-        typed = new Typed('article.code output', options);
+        typed = new Typed(window, options);
     });
     document.querySelector("img.csharp").addEventListener("click", event => {
-        typed.stop();
         typed.destroy();
         code = [csharp1];
         options = {
@@ -67,10 +68,9 @@ window.addEventListener("load", event => {
             smartBackspace: true,
             typeSpeed: 100
         };
-        typed = new Typed('article.code output', options);
+        typed = new Typed(window, options);
     });
     document.querySelector("img.js").addEventListener("click", event => {
-        typed.stop();
         typed.destroy();
         code = [js1];
         options = {
@@ -81,10 +81,9 @@ window.addEventListener("load", event => {
             smartBackspace: true,
             typeSpeed: 100
         };
-        typed = new Typed('article.code output', options);
+        typed = new Typed(window, options);
     });
     document.querySelector("img.dart").addEventListener("click", event => {
-        typed.stop();
         typed.destroy();
         code = [dart1];
         options = {
@@ -95,7 +94,7 @@ window.addEventListener("load", event => {
             smartBackspace: true,
             typeSpeed: 100
         };
-        typed = new Typed('article.code output', options);
+        typed = new Typed(window, options);
     });
     document.querySelector("img.flutter").addEventListener("click", event => {
         typed.stop();
@@ -109,7 +108,44 @@ window.addEventListener("load", event => {
             smartBackspace: true,
             typeSpeed: 100
         };
-        typed = new Typed('article.code output', options);
+        typed = new Typed(window, options);
     });
+
+    document.querySelector("img.html").addEventListener("click", event => {
+        typed.stop();
+        typed.destroy();
+        code = [html1];
+        options = {
+            contentType: 'null',
+            strings: code,
+            backSpeed: 100,
+            shuffle:true,
+            loop: true,
+            smartBackspace: true,
+            typeSpeed: 100
+        };
+        typed = new Typed(window, options);
+    });
+
+    // Window change
+    document.querySelector("img.windows").addEventListener("click", event => {
+        document.querySelector("#terminal").classList = "window-windows";
+        document.querySelector("#titlebar").classList = "titlebar-windows";
+        document.querySelector("#minimize").classList = "maximize";
+    });
+
+    document.querySelector("img.mac").addEventListener("click", event => {
+        document.querySelector("#terminal").classList = "window-mac";
+        document.querySelector("#titlebar").classList = "titlebar-mac";
+        document.querySelector("#minimize").classList = "minimize";
+    });
+
+    document.querySelector("img.linux").addEventListener("click", event => {
+        document.querySelector("#terminal").classList = "window-linux";
+        document.querySelector("#titlebar").classList = "titlebar-linux";
+    });
+
+
+
 
 });
